bd9fd24bde8940d4e1549ff172169b844e3755ea
Mohit kumar <mohitmkkumar98@gmail.com>
2021-12-25 14:57:26 +0530

    Started admin dashboard

---
+++ README.md
@@ -0,0 +1,57 @@
+# Introduction
+
+> _"Knowledge is the only thing that grows when shared."_ 🔥  
+
+
+<br>
+
+>## hy-crm
+Clint and Admin Dashboard
+
+## Table of Contents
+
+1. [Install](#install)
+2. [Technology Stack](#Technology-Stack)
+4. [Bugs and Feature Requests](#Bugs-and-Feature-Requests)
+5. [Contributing](#contributing)
+6. [Mentors](#mentors)
+
+
+<h2 align="center">Install</h2>
+
+```
+# Clone the repository
+# Install dependencies/required softwares
+$ cd hy-crm
+
+# Start application with
+$ php -S localhost:3000 (use any localhost port) 
+
+# Visit localhost:3000 or your entered port in your browser
+```
+
+<h2 align="center">Technology Stack</h2>
+
+- **HTML, CSS, Javascript, Bootstrap,JavaScript** 
+ 
+
+
+
+
+<h2 align="center">Bugs and Feature Requests</h2>
+
+Have a bug or a feature request? Please first read the [issue guidelines](https://github.com/mohitk30/hy-crm/blob/develop/Contributing.md) and search for existing and closed issues. If your problem or idea is not addressed yet, [please open a new issue](https://github.com/mohitk30/hy-crm/issues/new).
+
+<h2 align="center">Contributing</h2>
+
+We want contributing to Opportunity Calendar to be enjoyable and educational for everyone. We would love to have your contributions.
+To get started have a look at our [documentation on contributing](https://github.com/mohitk30/hy-crm/blob/develop/Contributing.md).
+
+<h2 align="center">Mentors</h2>
+
+| GitHub Usernames                                     | Domain                              |
+| ---------------------------------------------------- | ----------------------------------- |
+| [@mohitk30](https://github.com/mohitk30)             | Frontend                            |
+ 
+
+
---
+++ dashboard/admin/admin.php
@@ -0,0 +1,143 @@
+<!-- admin dashboard -->
+ 
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+    <!-- css -->
+    <link rel="stylesheet" type="text/css" href="styleadmin.css">
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <!-- font awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <!-- script -->
+    <script src='main.js'>
+    </script>
+    <!-- site icon -->
+    <link rel="icon" href="images/siteicon.png" type="image/gif" sizes="50x50">
+    <!-- animinate -->
+    <link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
+  />
+
+
+    <title>hycrm-admin</title>
+</head>
+<body>
+    <section id="main-admin-section">
+         <div id="main-container-div" class="d-flex flex-row">
+
+         <div id="left-container" class="">
+             <!-- all left options div -->
+          
+             <div class="left-options-div">
+                <!-- <div class="left-image-div px-sm-1 px-lg-2  " > <img class="left-icon-image" src="https://assets-geys.sgp1.digitaloceanspaces.com/growtheye/logo.png" alt="Logo"></div> -->
+                <div class="left-div-text mx-2"> <span class="align-middle text-white" > hy-crm Admin</span> </div>
+              </div>
+              
+              <a href="">
+             <div class="left-options-div left-options-div1">
+                <div class="left-image-div px-sm-1 px-lg-2  " data-bs-toggle="tooltip" data-bs-html="true" title="Dashboard" > <i class="fas fa-tachometer-alt fa-2x left-icon"></i></div>
+                <div class="left-div-text mx-2"> <span class="align-middle text-white" > Dashboard</span> </div>
+              </div>
+              </a>
+
+              <a href="">
+             <div class="left-options-div left-options-div1">
+                <div class="left-image-div px-sm-1 px-lg-2  " data-bs-toggle="tooltip" data-bs-html="true" title="Leads" > <i class="fas fa-book fa-2x left-icon"></i></div>
+                <div class="left-div-text mx-2"> <span class="align-middle text-white" > Leads</span> </div>
+              </div>
+              </a>
+
+              <a href="">
+             <div class="left-options-div left-options-div1">
+                <div class="left-image-div px-sm-1 px-lg-2  " data-bs-toggle="tooltip" data-bs-html="true" title="Analytics" > <i class="fas fa-chart-bar fa-2x left-icon"></i></div>
+                <div class="left-div-text mx-2"> <span class="align-middle text-white" > Analytics</span> </div>
+              </div>
+              </a>
+
+              <a href="">
+             <div class="left-options-div left-options-div1">
+                <div class="left-image-div px-sm-1 px-lg-2  " data-bs-toggle="tooltip" data-bs-html="true" title="Reports" > <i class="fas fa-box fa-2x left-icon"></i></div>
+                <div class="left-div-text mx-2"> <span class="align-middle text-white" > Reports</span> </div>
+              </div>
+              </a>
+
+              <a href="">
+             <div class="left-options-div left-options-div1">
+                <div class="left-image-div px-sm-1 px-lg-2  " data-bs-toggle="tooltip" data-bs-html="true" title="New Lead" > <i class="fas fa-plus-circle fa-2x left-icon"></i></div>
+                <div class="left-div-text mx-2"> <span class="align-middle text-white" > New Lead</span> </div>
+              </div>
+              </a>
+
+              <a href="">
+             <div class="left-options-div left-options-div1">
+             
+                <div class="left-image-div px-sm-1 px-lg-2  " data-bs-toggle="tooltip" data-bs-html="true" title="Notifications" ><span class="small-red-dot"></span>  <i class="fas fa-bell fa-2x left-icon">  </i> </div>
+                <div class="left-div-text mx-2"> <span class="align-middle text-white" > Notifications(0)</span> </div>
+              </div>
+              </a>
+
+              <a href="">
+             <div class="left-options-div left-options-div1">
+                <div class="left-image-div px-sm-1 px-lg-2  " data-bs-toggle="tooltip" data-bs-html="true" title="Settings" > <i class="fas fa-cog fa-2x left-icon"></i></div>
+                <div class="left-div-text mx-2"> <span class="align-middle text-white" > Settings</span> </div>
+              </div>
+              </a>
+              
+               
+
+
+
+
+<!-- user div -->
+              <a class="user-div" href="">
+             <div class="left-options-div left-options-div1 " data-bs-toggle="tooltip" data-bs-html="true" title="User">
+                <div class="left-image-div px-sm-1 px-lg-2  " > <i class="fas fa-user fa-2x left-icon"></i></div>
+                <div class="left-div-text mx-2"> <span class="align-middle text-white" > User</span> </div>
+              </div>
+              </a>
+
+               
+             
+               
+ 
+         </div>
+         <div id="right-container">
+  
+         </div>
+
+
+         </div>
+
+     
+      
+
+
+
+
+
+
+
+   
+
+
+    </section>
+
+
+
+
+
+</body>
+
+ 
